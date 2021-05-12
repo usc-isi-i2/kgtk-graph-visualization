38e275444a64898e56dbe48df28c4aacaf13d54a
g1eb <mail@g1eb.com>
2021-05-12 16:16:00 -0700

    Add application config file, make sure we can run this app with python

--- kgtk_browser_app.py
+++ kgtk_browser_app.py
@@ -2,6 +2,8 @@ import flask
 import requests
 from flask import render_template, jsonify
 
+from app_config import host, port
+
 
 # How to run:
 # > export FLASK_APP=kgtk_browser_app.py
@@ -34,3 +36,7 @@ def search(search_text):
     #response = requests.get("https://dsbox02.isi.edu:8888/kgtk-browser/kgtk/browser/backend/get_all_node_data?node={node_no}&lang=en&images=true&fanouts=true".format(node_no=node_no))
     response = requests.get("https://kgtk.isi.edu/api?q={search_text}&extra_info=true&language=en&is_class=false&type=ngram&size=5".format(search_text=search_text))
     return jsonify(response.json())
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
