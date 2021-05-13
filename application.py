6af0249fb9e3705f1075f52551288427692ddca3
g1eb <mail@g1eb.com>
2021-05-13 16:37:19 -0700

    Add /graph prefix to all of the urls in the app

--- application.py
+++ application.py
@@ -19,12 +19,12 @@ app.static_url_path='/static'
 
 ### URL handlers:
 
-@app.route('/<node>', methods=['GET'])
+@app.route('/graph/<node>', methods=['GET'])
 def index(node):
     return render_template("index.html", value = node)
 
 
-@app.route('/api/<node>', methods=['GET'])
+@app.route('/graph/api/<node>', methods=['GET'])
 def api(node):
     response = requests.get("https://dsbox02.isi.edu:8888/kgtk-browser/kgtk/browser/backend/get_all_node_data?node={node}&lang=en&images=true&fanouts=true".format(node=node))
     return jsonify(response.json())
