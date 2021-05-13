719547cc95ba6ed4fd69824cfa23cc1ddf3f9845
Shreya <>
2021-05-13 00:52:32 -0700

    Remove search from template and api backend

--- kgtk_browser_app.py
+++ kgtk_browser_app.py
@@ -21,19 +21,13 @@ app.static_url_path='/static'
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", value = "Q28022144")
 
 
 @app.route('/api/<node_no>', methods=['GET'])
 def api(node_no):
     response = requests.get("https://dsbox02.isi.edu:8888/kgtk-browser/kgtk/browser/backend/get_all_node_data?node={node_no}&lang=en&images=true&fanouts=true".format(node_no=node_no))
     return jsonify(response.json())
-
-@app.route('/search/<search_text>', methods=['GET'])
-def search(search_text):
-    response = requests.get("https://kgtk.isi.edu/api?q={search_text}&extra_info=true&language=en&is_class=false&type=ngram&size=5".format(search_text=search_text))
-    return jsonify(response.json())
-
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
