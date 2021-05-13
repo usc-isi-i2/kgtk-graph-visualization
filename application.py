b98874619fd4deb45b50b990343134d37481da82
g1eb <mail@g1eb.com>
2021-05-13 16:32:48 -0700

    Rename node_no to node to be more consistent

--- application.py
+++ application.py
@@ -24,10 +24,11 @@ def index(node):
     return render_template("index.html", value = node)
 
 
-@app.route('/api/<node_no>', methods=['GET'])
-def api(node_no):
-    response = requests.get("https://dsbox02.isi.edu:8888/kgtk-browser/kgtk/browser/backend/get_all_node_data?node={node_no}&lang=en&images=true&fanouts=true".format(node_no=node_no))
+@app.route('/api/<node>', methods=['GET'])
+def api(node):
+    response = requests.get("https://dsbox02.isi.edu:8888/kgtk-browser/kgtk/browser/backend/get_all_node_data?node={node}&lang=en&images=true&fanouts=true".format(node=node))
     return jsonify(response.json())
+
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
